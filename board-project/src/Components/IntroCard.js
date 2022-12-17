f419fadd0786643ed429b88ad9f17188fe020752
Ahmad Hassoun <ahmadhss366@gmail.com>
2022-12-17 12:58:27 +0200

    Adding main Page

--- board-project/src/Components/IntroCard.js
+++ board-project/src/Components/IntroCard.js
@@ -2,11 +2,10 @@ import React from 'react'
 
 function IntroCard() {
   return (
-    <div className='ml-48'>
-    <div className='shadow-lg rounded-lg w-96 h-96'>
-    <img src=''></img>
-    Stay on track and up to date
-    </div>  
+    <div className='ml-72 felx flex-row mt-2 '>
+
+ 
+
     </div>
   )
 }
